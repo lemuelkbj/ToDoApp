0a9e6a5b395a07a59788324644bc6b77865482ea
Lemuel <Lemuel@DESKTOP-2RBSBLV>
2022-03-27 20:16:26 +0530

    # WARNING: head commit changed in the meantime
    Merge branch 'Application-Development' of
    https://github.com/lemuelkbj/ToDoApp.git

--- src/main/java/com/webmancorp/TodoApp/Controller/ControllerOne.java
+++ src/main/java/com/webmancorp/TodoApp/Controller/ControllerOne.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webmancorp.TodoApp.Entities.ToDoTask;
@@ -23,5 +24,10 @@ public class ControllerOne {
 	@GetMapping("/getTasks")
 	public List<ToDoTask> getTasks(){
 		return this.obj.getTasks();
+	}
+	
+	@GetMapping("/getTasks/{priority}")
+	public List<ToDoTask> getPriority(@PathVariable String priority) {
+		return this.obj.getPriority(priority);
 	}
 }
